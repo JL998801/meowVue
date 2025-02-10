2c157b77d4f20bd7af855abbb0510fff42c69d9e
JC <sonicchu1223@hotmail.com>
2025-02-10 16:52:56 +0800

    管理員token

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import routes from './router/routers.js'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import FontAwesomeIcon from './plugins/fontawesome.js'
+
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate)
@@ -11,5 +11,4 @@ pinia.use(piniaPluginPersistedstate)
 createApp(App)
     .use(pinia)
     .use(routes)
-    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
