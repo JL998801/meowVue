7edfc085c8c316cf0852015c8669d5e9e6ffd603
KuanJu <ab82825519@gmail.com>
2025-02-11 22:39:43 +0800

    ???

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -11,9 +11,5 @@ pinia.use(piniaPluginPersistedstate)
 createApp(App)
     .use(pinia)
     .use(routes)
-<<<<<<< HEAD:vue-project/src/main.js
-    .mount('#app')
-=======
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
->>>>>>> 683414455919d46f5fa0828d9c3d6d258ab97f92:src/main.js
