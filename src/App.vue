f813176f0c24e7dc4f258f44dd1ae5518eada3a6
KUANJULEE <ab82825519@gmail.com>
2025-02-11 19:28:55 +0800

    修改完管理員登入後驗證邏輯

--- src/App.vue
+++ src/App.vue
@@ -64,6 +64,7 @@ const isFullWidth = computed(() => fullWidthRoutes.includes(route.path));
   background-color: #ffffff;
   padding: 0 70px;
   margin: 0 auto;
+  background-image: url('@/assets/背景圖2.png');
 }
 
 /* 當 `isFullWidth` 為 true，讓 `.container` 變成全寬而且不要有卷軸 */
