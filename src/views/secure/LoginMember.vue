9f77d718b6cb70f34b7b6771aa39b05f303a4180
KUANJULEE <ab82825519@gmail.com>
2025-02-11 11:21:55 +0800

    修正登入後跳轉路徑(/pets/MemberCenter)

--- src/views/secure/LoginMember.vue
+++ src/views/secure/LoginMember.vue
@@ -170,13 +170,13 @@ async function submitForm() {
       // 儲存登入資訊到 localStorage，同時儲存到pinia中(原檔案邏輯)
       saveUserInfoToLocalStorage(response.data.user, response.data.token);
       userStore.setEmail(response.data.user.email);
-      userStore.setToken(response.data.token);
+      // userStore.setToken(response.data.token);
 
       // 設定 authorization header
       xxx.defaults.headers.authorization = `Bearer ${response.data.token}`;
 
       // 跳轉到會員中心
-      router.push({ path: "/pages/MemberCenter" });
+      router.push({ path: "/pets/MemberCenter" });
     } else {
       message.value = response.data.message;
       Swal.fire({
